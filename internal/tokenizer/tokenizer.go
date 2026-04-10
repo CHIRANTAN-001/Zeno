a5902be987c56439d943b1cd71fa0e49c04f875c
Chirantan <daschirantan0002@gmail.com>
2026-04-11 04:18:06 +0530

    feat(tokenizer): clean tokens by removing non-alphanumeric characters for better indexing

--- internal/tokenizer/tokenizer.go
+++ internal/tokenizer/tokenizer.go
@@ -1,18 +1,20 @@
 package tokenizer
 
 import (
-	"strings"
-
 	"github.com/CHIRANTAN-001/zeno/internal/stopwords"
+	"regexp"
+	"strings"
 )
+
+var nonAlpha = regexp.MustCompile("[^a-z0-9]+")
 
 func Tokenize(text string, filter *stopwords.Filter) []string {
 	words := strings.Fields((strings.ToLower(text)))
-
 	var result []string
 
 	for _, word := range words {
-		if !filter.IsStopWord(word) {
+		word = nonAlpha.ReplaceAllString(word, "")
+		if word != "" && !filter.IsStopWord(word) {
 			result = append(result, word)
 		}
 	}
